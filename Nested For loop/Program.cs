e94b5b1ae6453f19d27f91985fabae02f149da25
boriskisiov <boriskisiov@gmail.com>
2023-10-24 14:41:04 +0300

    Add project files.

---
+++ Nested For loop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Nested_For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int colums = 2;
+            for (int r = 1; r <= rows; r++) {
+                Console.WriteLine("row = " + r);
+                for (int c = 1; c <= colums; c++)
+                {
+                    Console.WriteLine(" colums" +  c);
+                }
+            }
+        }
+    }
+}
